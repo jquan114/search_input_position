328af7e6502c9ce42239c41dc67eeb6570ac6644
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-05 23:04:03 -0500

    Update inputPosition.js

--- inputPosition.js
+++ inputPosition.js
@@ -1,6 +1,6 @@
-Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-You must write an algorithm with O(log n) runtime complexity.
+// You must write an algorithm with O(log n) runtime complexity.
 
  function searchInsert(nums, target) {
      for ( let i = 0 ; i < nums.length ; i++){
@@ -10,3 +10,5 @@ You must write an algorithm with O(log n) runtime complexity.
      }
     return nums.length;
 };
+
+searchInsert([1,3,5,6]])
